38efecdba63f4a0864addb51219063b2d6c7f958
saraani <ummulsaraani@gmail.com>
2022-10-04 19:40:10 +0530

    tic-tac-toe

---
+++ src/components/Reset.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Reset.css'
+
+function Reset({resetBoard}) {
+  return (
+   <button className='reset-btn' onClick={resetBoard}>Reset</button>
+  )
+}
+
+export default Reset
